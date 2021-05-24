0b7df17a8208a6cd3b12891d6f4edfbc37fe1ea5
Piotr Krzyminski <piotrkrzyminski96@gmail.com>
2021-05-24 09:37:10 +0200

    Initial spring boot application

---
+++ src/main/java/pl/krzyminski/airqualityservice/AirqualityServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.krzyminski.airqualityservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirqualityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirqualityServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/krzyminski/airqualityservice/AirqualityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.krzyminski.airqualityservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirqualityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
